ba80ff68061c5a7fca56f59f911bafd766deec27
Nasser <nasser.m.95@gmail.com>
2025-03-13 10:46:33 +0400

    re-adding index for github pages

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function showMessage() {
-    document.getElementById("message").innerText = "Hello, World! You clicked the button!";
+    document.getElementById("message").innerText = "Super Nas!";
 }
